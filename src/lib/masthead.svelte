85eb627f8d4ef6051202403df1944ee8b6a89d33
EdwardLinS <63514282+EdwardLinS@users.noreply.github.com>
2023-01-18 22:14:03 -0800

    add background image, buttons, and logo

--- src/lib/masthead.svelte
+++ src/lib/masthead.svelte
@@ -1,18 +1,41 @@
-<div class="background"></div>
-<h1 id="titleBlurb">Compassionate Care for Community Cats</h1>
-<p id="textBlurb">Reducing the population of homeless cats in Los Angeles
-    in the most humane way, start your TNR journey today.</p>
+<script>
+    import BackgroundCat from "$lib/assets/img/background_cat.png"
+    import FixNationLogo from "$lib/assets/img/fixnation_logo.svg"
+</script>
 
+<div>
+    <img src={BackgroundCat} alt="black and white cat on the ground" id="background"/>
+    <img src={FixNationLogo} alt="logo for fixnation" id="logo"/>
+    <div id="titleBlob"/>
+    <h1 id="titleBlurb">Compassionate Care for Community Cats</h1>
+    <p id="textBlurb">Reducing the population of homeless cats in Los Angeles
+        in the most humane way, start your TNR journey today.</p>
+    <button id="involveButton">Get Involved</button>
+    <button id="donateButton">Donate Now</button>
+</div>
 
 <style lang="css">
-    .background {
+
+    #background {
         position: absolute;
-        height: 100%;
         width: 100%;
-        top: 0;
-        background-image: "../../static/masthead/background_cat.png";
+        height: auto;
+        top: 0px;
         z-index: -1;
+        box-shadow: inset 0px 200px 200px 100px rgba(0, 0, 0, 0.4);
     }
+
+    #logo {
+        margin: auto;
+        display: block;
+    }
+
+    #titleBlob {
+        left: 0px;
+
+        background-image: url("assets/title_blob.svg");
+    }
+
 
     #titleBlurb {
         font-family: 'ITC Avant Garde Gothic LT Demi';
