3af1804c4840a8958b019cad40c938e84674cbf8
EdwardLinS <63514282+EdwardLinS@users.noreply.github.com>
2023-01-18 21:17:11 -0800

    move masthead icons

--- src/lib/masthead.svelte
+++ src/lib/masthead.svelte
@@ -1,20 +1,40 @@
+<div class="background"></div>
 <h1 id="titleBlurb">Compassionate Care for Community Cats</h1>
 <p id="textBlurb">Reducing the population of homeless cats in Los Angeles
     in the most humane way, start your TNR journey today.</p>
 
+
 <style lang="css">
+    .background {
+        position: absolute;
+        height: 100%;
+        width: 100%;
+        top: 0;
+        background-image: "../../static/masthead/background_cat.png";
+        z-index: -1;
+    }
+
     #titleBlurb {
         font-family: 'ITC Avant Garde Gothic LT Demi';
         font-style: normal;
         font-weight: 700;
         font-size: 48px;
         line-height: 150%;
-        /* or 72px */
 
         letter-spacing: 0.02em;
 
-        /* Functional/Surface */
-
         color: #FFFFFF;
+    }
+
+    #textBlurb {
+        font-family: 'Avenir';
+        font-style: normal;
+        font-weight: 500;
+        font-size: 24px;
+        line-height: 150%;
+
+        letter-spacing: 0.02em;
+
+        color: #F9F9F9;
     }
 </style>
