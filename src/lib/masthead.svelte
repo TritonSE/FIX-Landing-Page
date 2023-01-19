c3b7fef6f16d24b8e89014f29487a20ca4944911
EdwardLinS <63514282+EdwardLinS@users.noreply.github.com>
2023-01-18 21:00:50 -0800

    add icons for masthead

--- src/lib/masthead.svelte
+++ src/lib/masthead.svelte
@@ -1,1 +1,20 @@
-<>
+<h1 id="titleBlurb">Compassionate Care for Community Cats</h1>
+<p id="textBlurb">Reducing the population of homeless cats in Los Angeles
+    in the most humane way, start your TNR journey today.</p>
+
+<style lang="css">
+    #titleBlurb {
+        font-family: 'ITC Avant Garde Gothic LT Demi';
+        font-style: normal;
+        font-weight: 700;
+        font-size: 48px;
+        line-height: 150%;
+        /* or 72px */
+
+        letter-spacing: 0.02em;
+
+        /* Functional/Surface */
+
+        color: #FFFFFF;
+    }
+</style>
