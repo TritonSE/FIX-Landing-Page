2fdbea534d533038e586708fd824556675120402
EmilyMarschallNiswonger <emilymarschallniswonger@gmail.com>
2023-01-20 15:48:42 -0800

    Format for cat stories

--- src/lib/cat_stories/cat_image.svelte
+++ src/lib/cat_stories/cat_image.svelte
@@ -7,5 +7,6 @@
 <style>
   img {
     filter: drop-shadow(16px 16px 0px #58c3af);
+    width: 100%;
   }
 </style>
--- src/lib/cat_stories/index.svelte
+++ src/lib/cat_stories/index.svelte
@@ -9,7 +9,42 @@
   <h2>We’re feral, stray or owned pet cat who used to roam the outdoors. FixNation found us!</h2>
   <div class="stories-grid">
     <CatImage image_url="/cat_stories/shadow.png" />
-    <Paw name="Shadow" />
+    <div class="column">
+      <Paw name="Shadow" />
+      <p class="medium-margin-above">
+        I’m the queen of the Home Depot in Woodland Hills, where I serve as the store’s resident
+        mouser. I came to FixNation for spay surgery and was returned the next day to my home turf,
+        where I’m back to patrolling the aisles of the garden center and making customers smile.
+        I’ve been told I’m an excellent example of a healthy and thriving working cat.
+      </p>
+      <p class="underline-text">See more</p>
+    </div>
+    <div class="column large-gap">
+      <CatImage image_url="/cat_stories/close_up.png" />
+      <div class="row">
+        <Paw name="Close Up" />
+        <div class="column">
+          <p>
+            Nice to meet you! I’m your well-known resident of Hollywood Forever. You can find me
+            sunning on...
+          </p>
+          <p class="underline-text">See more</p>
+        </div>
+      </div>
+    </div>
+    <div class="column large-gap">
+      <CatImage image_url="/cat_stories/almira.png" />
+      <div class="row">
+        <Paw name="Almira" />
+        <div class="column">
+          <p>
+            I’m one of the cats at Jay Leno’s famous garage in the San Fernando Valley which is just
+            a stone’s throw from our clinic. FixNation has...
+          </p>
+          <p class="underline-text">See more</p>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
 
@@ -27,8 +62,40 @@
     margin: 0;
   }
 
+  p {
+    font-size: 16px;
+    font-weight: 500;
+    text-align: left;
+    margin: 0px;
+  }
+
   .stories-grid {
     display: grid;
     grid-template-columns: 1fr 1fr;
+    gap: 7%;
+    padding: 80px 160px 80px 160px;
+  }
+
+  .column {
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+  }
+
+  .large-gap {
+    gap: 35px;
+  }
+
+  .medium-margin-above {
+    margin-top: 36px;
+  }
+
+  .row {
+    display: flex;
+    flex-direction: row;
+    gap: 15px;
+  }
+  .underline-text {
+    text-decoration-line: underline;
   }
 </style>
