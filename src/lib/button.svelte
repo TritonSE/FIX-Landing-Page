ba3153bf52e9a1a20cea4778d6b20207165cdde0
Jacob Au <jacobau.inbox@gmail.com>
2023-01-21 14:16:46 -0800

    first commit

---
+++ src/lib/button.svelte
@@ -0,0 +1,34 @@
+<script>
+	let buttonProps = {
+	    class:[$$restProps.class]
+	}
+</script>
+
+<button on:click>
+    <slot/>
+</button>
+
+<style>
+
+    button {
+        background-color: #58c3af;
+        color:white;
+        width:100px;
+        height:48px; 
+        font-size: 16px;
+        border-radius: 4px;
+    }
+
+    button:hover {
+        background-color: #1f9c8e;
+    }
+
+    button:disabled {
+        background-color: #149788;
+    }
+
+    button:active{
+        background-color: #F0F4F4;
+    }
+
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
+<script>
+    import Button from '../lib/button.svelte';
+</script>
+    
+<style global>
+/* CSS controlling font sizes and faces here */
+    @font-face {
+    font-family: 'Material Icons';
+    font-style: normal;
+    font-weight: 400;
+    src: url(https://example.com/MaterialIcons-Regular.eot); /* For IE6-8 */
+    src: local('Material Icons'),
+        local('MaterialIcons-Regular'),
+        url(https://example.com/MaterialIcons-Regular.woff2) format('woff2'),
+        url(https://example.com/MaterialIcons-Regular.woff) format('woff'),
+        url(https://example.com/MaterialIcons-Regular.ttf) format('truetype');
+    }
+
+</style>
+<Button>this is a button</Button>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
