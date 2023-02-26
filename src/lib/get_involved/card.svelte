40d20b3cb20d32325f310553b431e06d4cda5f92
Andrew <Cubified@users.noreply.github.com>
2023-02-25 17:16:32 -0800

    Update card.svelte

--- src/lib/get_involved/card.svelte
+++ src/lib/get_involved/card.svelte
@@ -17,7 +17,6 @@
     // Crude touchscreen detection via:
     //   https://stackoverflow.com/questions/4817029/whats-the-best-way-to-detect-a-touch-screen-device-using-javascript
     mobile = !!('ontouchstart' in window || navigator.maxTouchPoints > 0);
-    console.log(mobile);
   };
   onMount(detectMobile);
 </script>
