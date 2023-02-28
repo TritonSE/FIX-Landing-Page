7ac8d268912fe15fc21ed855ffc1a3855d4d2e2d
LillianHo5 <92070679+LillianHo5@users.noreply.github.com>
2023-02-28 00:54:57 -0800

    Added thank you screen after signing up for the newsletter

--- src/lib/footer.svelte
+++ src/lib/footer.svelte
@@ -8,8 +8,20 @@
   import { fly } from 'svelte/transition';
 
   let visible = false;
+  let showForm = false;
+  let submitted = false;
   let name = '';
   let email = '';
+
+  // Function to validate name and email fields before submitting the form
+  function handleSubmit() {
+    if (name.length > 0 && email.length > 0 && email.includes('@')) {
+      submitted = true;
+      showForm = false;
+    } else {
+      // Handle case where both fields are not submitted
+    }
+  }
 </script>
 
 <div class="container">
@@ -19,7 +31,7 @@
       <Button secondary small fill>FixNation Website</Button>
     </a>
     <div class="spacer" />
-    <Button secondary small fill <button on:click={() => (visible = true)}>
+    <Button secondary small fill <button on:click={() => ((visible = true), (showForm = true))}>
       Sign up for Newsletter</Button
     >
     <p id="copyright">© 2020 FixNation, Inc. All Rights Reserved.</p>
@@ -31,18 +43,29 @@
   </div>
 
   {#if visible}
-    <div id="newsletter_input" transition:fly={{ x: 200, y: 200, duration: 500 }}>
-      <p>Sign up for our newsletter to stay in touch!</p>
-      <input id="name-input" bind:value={name} placeholder="Name" />
-      <input id="email-input" bind:value={email} placeholder="Email" />
-      <Button id="signup-button" small fill <button on:click={() => (visible = true)}
-        >Sign Up</Button
-      >
-    </div>
     <div class="animated_blob-container" transition:fly={{ x: 200, y: 200, duration: 500 }}>
       <div id="animated_blob">
         <img src={animated_blob} alt="animated footer blob" id="animated_footer_blob" />
       </div>
+    </div>
+  {/if}
+  {#if showForm}
+    <div id="newsletter_input" transition:fly={{ x: 200, y: 200, duration: 500 }}>
+      <p>Sign up for our newsletter to stay in touch!</p>
+      <input id="name-input" bind:value={name} placeholder="Name" />
+      <input id="email-input" bind:value={email} placeholder="Email" />
+      <Button id="signup-button" small fill <button on:click={handleSubmit}>Sign Up</Button>
+    </div>
+  {/if}
+  {#if submitted}
+    <div id="thank-you">
+      <h1>Thank you,<br />{name}!</h1>
+      <Button
+        small
+        fill
+        <button
+        on:click={() => ((visible = false), (showForm = false), (submitted = false))}>Done</Button
+      >
     </div>
   {/if}
 </div>
@@ -57,9 +80,14 @@
   input {
     margin: 0.5rem;
     text-align: center;
+    box-shadow: inset 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border: transparent;
+    height: 2vw;
+    width: 18vw;
   }
 
-  #newsletter_input {
+  #newsletter_input,
+  #thank-you {
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -70,17 +98,15 @@
     right: 40vw;
   }
 
+  #thank-you {
+    text-align: center;
+    color: white;
+  }
+
   #newsletter_input p {
     white-space: nowrap;
     color: var(--color-white);
     font-weight: 900 !important;
-  }
-
-  #newsletter_input input {
-    box-shadow: inset 0px 4px 4px rgba(0, 0, 0, 0.25);
-    border: transparent;
-    height: 2vw;
-    width: 18vw;
   }
 
   #name-input {
@@ -121,15 +147,6 @@
     right: 0;
     z-index: -3;
   }
-
-  /* #blob_close {
-    position: absolute;
-    top: 70px;
-    right: 20px;
-    border: none;
-    background-color: transparent;
-    z-index: 1;
-  } */
 
   #logo {
     margin-right: 0.5rem;
