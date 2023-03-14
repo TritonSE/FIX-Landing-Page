14cdb828fd7ab98f69e4c1a718818e5148721966
Melina Dimitropoulou <mdimitropoulouka@ucsd.edu>
2023-03-13 22:44:15 -0700

    about section

---
+++ src/lib/about.svelte
@@ -0,0 +1,50 @@
+<script>
+  import header_img from '../../static/about/about_header.png';
+</script>
+
+<h2>Thousands of cats are needlessly euthanized in Los Angeles each year,</h2>
+<p>
+  their only fault being that they don’t have a home. Los Angeles is home to one of the nation’s
+  largest populations of homeless cats with an estimated one to three million living on the streets.
+</p>
+<img src="/about/about_header.png" alt="cat header" />
+<div>
+  <h3>“We need to do something!”</h3>
+  <p>
+    Eager to find a solution to the problem, Karn Myers and her husband Mark Dodge launched
+    FixNation--the first full-time, permanent spay/neuter facility in Los Angeles. Now, their
+    award-winning programs serve as a model for other communities and charities dealing with similar
+    situations on a global scale.
+  </p>
+  <div>
+    <img src="/about/top_blob.svg" alt="blob accent" />
+    <img src="/about/top_image.png" alt="volunteering" />
+  </div>
+</div>
+<div>
+  <h3>For the love of cats</h3>
+  <p>
+    At the heart of our high-volume, high-quality spay/neuter clinic is an experienced and expert
+    team that genuinely cares about cats. In addition to free spay/neuter surgery, every homeless
+    cat brought to FixNation receives vaccinations, fluids, antibiotics, pain medication and flea
+    treatment.
+  </p>
+  <div>
+    <img src="/about/bottom_blob.svg" alt="blob accent" />
+    <img src="/about/bottom_image.png" alt="love cats" />
+  </div>
+</div>
+<div>
+  <h3>Our mission</h3>
+  <p>
+    Our mission is to humanely reduce the population of homeless cats in Los Angeles by sterilizing
+    as many as possible and allowing their numbers to naturally decline over time.
+  </p>
+  <div>
+    <img src="/about/logo_blob.svg" alt="blob accent" />
+    <img src="/about/logo.svg" alt="Fix Nation logo" />
+  </div>
+</div>
+
+<style>
+</style>
