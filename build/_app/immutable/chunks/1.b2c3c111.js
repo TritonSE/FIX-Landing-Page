7f6961f517775302095f676826f6b3dd2d2e6973
Andrew <Cubified@users.noreply.github.com>
2023-06-03 16:05:20 -0700

    Clean up directory structure

---
+++ build/_app/immutable/chunks/1.b2c3c111.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.488b1639.js";export{t as component};
--- build/_app/immutable/chunks/2.98bb0cc6.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.9d94d7b6.js";export{t as component};
